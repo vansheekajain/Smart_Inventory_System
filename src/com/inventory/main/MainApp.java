44d361f0d7d7ab44a4b045948176f13ec89c4c09
vansheekajain <vansheekajain26@gmail.com>
2026-03-29 08:30:44 +0530

    Create MainApp.java

---
+++ src/com/inventory/main/MainApp.java
@@ -0,0 +1,9 @@
+package com.inventory.main;
+
+import com.inventory.ui.LoginFrame;
+
+public class MainApp {
+    public static void main(String[] args) {
+        new LoginFrame().setVisible(true);
+    }
+}
