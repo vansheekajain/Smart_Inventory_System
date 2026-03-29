039e991e0f20b88d1646345c0d6204931d3720bb
vansheekajain <vansheekajain26@gmail.com>
2026-03-29 08:44:08 +0530

    Create Supplier.java

---
+++ src/com/inventory/model/Supplier.java
@@ -0,0 +1,51 @@
+package com.inventory.model;
+
+public class Supplier {
+    private int id;
+    private String name;
+    private String contact;
+    private String address;
+
+    public Supplier() {}
+
+    public Supplier(int id, String name, String contact, String address) {
+        this.id = id;
+        this.name = name;
+        this.contact = contact;
+        this.address = address;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
